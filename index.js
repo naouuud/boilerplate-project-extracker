4c0fcb34c5cf3e67dfe449e1734d93b16f078317
Nour Aoude <naoude@gmail.com>
2024-01-26 15:01:44 +0200

    Fix db error handling 2

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }));
     await mongoose.connect(process.env.MONGO_URL);
     console.log("Database connected");
   } catch (err) {
-    next(err);
+    return next(err);
   }
 })().catch(console.log);
 
